b3ba46318eaa7f26b9bf4a257476a9d8982fba39
xiaoming-been <55220127+xiaoming-been@users.noreply.github.com>
2019-10-12 13:12:44 +0800

    Update work4.js

--- work4/work4.js
+++ work4/work4.js
@@ -1,7 +1,7 @@
 document.write("<p>金字塔</p>")
 let level = prompt('请设置金字塔的层数')
 level = parseFloat(level)&&Number(level)
-if (isNaN(lever)){
+if (isNaN(level)){
     alert('金字塔的层数是数字')
 }
 for(let i=1;i<=level;++i){
@@ -18,8 +18,8 @@ for(let i=1;i<=level;++i){
     document.write("<br>")
 }
 
-let str = 'table border="1">'
-for(let i=1;i<10;++j){
+let str = '<table border="1">'
+for(let i=1;i<10;++i){
     str += '<tr>'
     for(var j=1;j<=i;++j){
         //；拼接单元格
