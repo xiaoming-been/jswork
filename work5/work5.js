c2380ea6523e5879206daf363c820caa82b5a2e3
xiaoming-been <1294340611@qq.com>
2019-10-29 21:56:40 +0800

    work5

--- work5/work5.js
+++ work5/work5.js
@@ -1,3 +1,14 @@
+str ='<table border="1">'
+for (let i=1;i<=9;i++){
+    str +='<tr>';
+    for (var j=1;j<=9;j++){
+        str +='<td>&nbsp;</td>';
+    }
+    str +='</tr>';
+}
+    str += '</table>';
+    document.getElementById('table1').innerHTML=str
+
 
 str ='<table border="1">'
 for(let i=9;i>0;--i){
@@ -9,3 +20,15 @@ for(let i=9;i>0;--i){
 }
 str += '</table>'
 document.getElementById('table2').innerHTML = str
+
+
+str ='<table border="1">'
+for(let i=9;i>0;--i){
+    str += '<tr>'
+    for(var j=i;j>0;--j){
+       str +='<td>'+j+'*'+i+'='+(j*i)+'</td>';
+    }
+    str +='</tr>'
+}
+str += '</table>'
+document.getElementById('table3').innerHTML = str
