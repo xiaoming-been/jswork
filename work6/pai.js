6eb152f5b5bf6e6f658349fe0246934bd835de58
xiaoming-been <1294340611@qq.com>
2019-11-03 17:01:58 +0800

    work6

--- work6/pai.js
+++ work6/pai.js
@@ -2,29 +2,30 @@ function calculate(){
     let num = document.getElementById('num').value 
     num = parseInt(num)&&Number(num)
     if(!isNaN(num)){
-        document.getElementById('num').value = pi(sum)
+        document.getElementById('pai').value = pi(num)
     }
 }
 function factorial(n){
     let sum = 1
     for(let i=1;i<=n;++i){
-        sum = calculate()
+        sum *=i
     }
     return sum
 }
 function oddFactorial(n){
     let sum = 1
     for(let i=1;i<=n;++i){
-        sum = oddFactorial()
+        sum *=2*i+1
     }
     return sum
 }
 function pi(num){
     let sum = 0
     for(let i=0;i<=num;++i){
-        let dividend = factorial()
-        let divisor = oddFactorial()
+        let dividend = factorial(i)
+        let divisor = oddFactorial(i)
+        sum += (dividend/divisor);
     }
-    return sum = 1/2
+    return sum *2
 }
     calculate()
--- work7/utils.js
+++ work7/utils.js
@@ -3,7 +3,7 @@ function bubbleSort(str) {
     for (let i = 0; i < arr.length - 1; i++) {
         for (let j = 0; j <arr.length - i - 1; j++){
             if (arr[j] > arr[j + 1]){
-                [arr[j], arr[j + 1]] = [arr[j + i], arr[j]]
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
             }
         }
     }
