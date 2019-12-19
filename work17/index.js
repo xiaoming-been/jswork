124a3dee2bb6dbab8fc98176b33f6680bf512421
xiaoming-been <1294340611@qq.com>
2019-12-19 19:12:26 +0800

    work17

--- work17/index.js
+++ work17/index.js
@@ -17,7 +17,7 @@ function inputBlur() {
         return false;
     }
 
-    var reg_msg = getRegMsg(name,tips);
+    var reg_msg = getRegMsg(name, tips);
 
     if (reg_msg['reg'].test(val)) {
 
@@ -33,5 +33,35 @@ function getRegMsg(name, tips) {
     switch (name) {
         case 'username':
             reg = /^[a-zA-Z]{4,12}$/;
+            msg = {'success': '用户名输入正确', 'error': tips};
+            break;
+        case 'pwd':
+            reg = /^\w{6,20}$/;
+            msg = {'success': '密码输入正确', 'error': tips};
+            break;
+        case 'repwd':
+            var con = document.getElementsByTagName('input')[1].value;
+            reg = RegExp("^" + con + "$");
+            msg = {'success': '两次密码输入正确', 'error': '两次输入的密码不一致'};
+            break;
+        case 'tel':
+            reg = /^1[34578]\d{9}$/;
+            msg = {'success': '手机号码输入正确', 'error': tips};
+            break;
+        case 'email':
+            reg = /^(\w+(\_|\-\.)*)+@(\w+(\-)?)+(\.\w{2,})+$/;
+            msg = {'success': '邮箱输入正确', 'error': tips};
+            break;
+            }
+        return {'reg': reg, 'msg': msg};
     }
-}
+
+    function success(obj, msg) {
+        obj.className = 'success';
+        obj.innerHTML = msg;
+    }
+
+    function error(obj, msg) {
+        obj.className = 'error';
+        obj.innerHTML = msg + '， 请重新输入';
+    }
