62203428c36f97f99ddd8dbc83e2289c646de618
xiaoming-been <1294340611@qq.com>
2019-12-24 22:30:56 +0800

    work18

--- work18/index.js
+++ work18/index.js
@@ -1,15 +1,26 @@
-(function () {
-    function PageList(options) {
-        for (var i in options) {
-            this[i] = options[i];
-        }
-        var obj = this;
-        this.first.onclick = function() {
-            obj.page = 1;
-            obj.onChange();
-        };
-        this.prev.onclick = function () {
-            obj.page = (obj.page > 1) ? (obj.page - 1) : 1;
-        }
+function send() {
+    let sno = document.getElementById('sno').value
+    let name = document.getElementById('name').value
+    let content = document.getElementById('content').value
+    let xmlhttp;
+    if (window.XMLHttpRequest) {
+
+        xmlhttp = new XMLHttpRequest();
+    } else {
+
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
-})();
+
+    //xmlhttp.open('post', 'http://localhost:8080/ajax', true)
+    xmlhttp.open('post', 'http://139.9.81.203:8090/ajax', true)
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.send('sno=' + sno + '&name=' + name+"&content="+content)
+
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            let ajaxData=JSON.parse(xmlhttp.responseText)
+            result = ajaxData.reverse().map((val)=>{return JSON.stringify(val)})
+            document.getElementById("result").innerHTML = result.join('<br>');
+        }
+    }   
+}
